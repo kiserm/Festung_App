0ec5955d9d659abaff458bab6adb2503bfea294e
kiserm <markus.kiser@hotmail.com>
2018-12-04 10:11:57 +0100

    up to tut4 cleaned

--- screens/TutorialScreen4.js
+++ screens/TutorialScreen4.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import {Text,View,TouchableHighlight,Animated,NativeModules} from 'react-native';
+import {Text,View,TouchableHighlight} from 'react-native';
 import styles from '../constants/Styles'; // for design purpose, import the styles from the self-made Style-Document
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconEntypo from 'react-native-vector-icons/Entypo';
-// to be able to have a proper design on every platform, I downloaded this package from 
-// this website: https://www.npmjs.com/package/react-native-responsive-screen
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen'; // to be able to have a proper design on every platform, I downloaded this package from this website: https://www.npmjs.com/package/react-native-responsive-screen
 
-
-export default class TutorialScreen2 extends React.Component {
-  // set a title for the navigation bar at the top and the design is in the file ../navigation/StackNavigator.js
+/**
+ * IDEA:
+ * this screen should introduce the user to the map screen. explain what it is and where the button can be found
+ */
+export default class TutorialScreen4 extends React.Component {  
   static navigationOptions = ({navigation}) => ({
-    title:'TutorialScreen2',
+    // set a title for the navigation bar at the top and the design is in the file ../navigation/StackNavigator.js
+    title:'TutorialScreen4',
     // show the map icon in red and bigger than the others, such that the user gets an intuition which button gets explained
     headerLeft:(<View style={styles.headerButtonContainer}>
                   <TouchableHighlight onPress={() => {}} style={styles.navigationBarIcons}>
@@ -31,60 +32,56 @@ export default class TutorialScreen2 extends React.Component {
                     </View>
     ),
   });
-    
-      render() {
 
+  render() {
+    return (      
+      <View style={styles.anyWholeScreen}>
+        
+        {/*Set the title of the Screen*/}        
+        <View style={styles.tutorialTitleTextContainer}>
+          <Text style={styles.tutorialTitleTextFormat}>
+            Tutorial
+          </Text>
+        </View>   
 
-        return (      
-          <View style={styles.anyWholeScreen}>
-            
-            {/*Set the title of the Screen*/}        
-            <View style={styles.tutorialTitleTextContainer}>
-              <Text style={styles.tutorialTitleTextFormat}>
-                Tutorial
-              </Text>
-            </View>     
-            <View style={styles.tutorialSubtitleContainer}>
-              <Text style={styles.tutorialSubtitleTextFormat}>
-                Schritt 3 von 3
-              </Text>
-            </View>
-            <View style={styles.tutorialTextContainer}>
-                <Text style={styles.stationTextFormat}>
-                  Wenn Du auf obiges rotes Symbol drückst, öffnet sich der Übersichtsplan der Festung Fürigen, der Dir
-                  als Orientierungshilfe dienen sollte.
-                </Text>
-            </View>
+        {/*Set the subtitle of the Screen*/}           
+        <View style={styles.tutorialSubtitleContainer}>
+          <Text style={styles.tutorialSubtitleTextFormat}>
+            Schritt 3 von 3
+          </Text>
+        </View>
 
-            {/* put three buttons at the bottom, back, how far i am in the tutorial and forward*/}
-            <View style={styles.tutorialBottomContainer}>
+        {/* explain the specific icon */}        
+        <View style={styles.tutorialTextContainer}>
+            <Text style={styles.stationTextFormat}>
+              Wenn Du auf obiges rotes Symbol drückst, öffnet sich der Übersichtsplan der Festung Fürigen, der Dir
+              als Orientierungshilfe dienen sollte.
+            </Text>
+        </View>
 
-              <View style={styles.stationRowContainer}>
-                <TouchableHighlight onPress={() => this.props.navigation.navigate('Tutorial3')} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
-                    <Text style={styles.anyButtonText}> Zurück </Text>
-                </TouchableHighlight>
-              </View>
+        {/* put three buttons at the bottom, back, how far i am in the tutorial and forward*/}
+        <View style={styles.tutorialBottomContainer}>
+          <View style={styles.stationRowContainer}>
+            <TouchableHighlight onPress={() => this.props.navigation.navigate('Tutorial3')} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
+                <Text style={styles.anyButtonText}> Zurück </Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.tutorialRowMiddleContainer}>
+            <TouchableHighlight onPress={()=>{}} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
+                <Text style={styles.anyButtonText}> 3 / 3 </Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.stationRowContainer}>
+            <TouchableHighlight onPress={() => this.props.navigation.navigate('Tutorial5')} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
+                  <Text style={styles.anyButtonText}>Weiter </Text>
+            </TouchableHighlight>
+          </View>
+        </View>
 
-             <View style={styles.tutorialRowMiddleContainer}>
-                <TouchableHighlight onPress={()=>{}} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
-                    <Text style={styles.anyButtonText}> 3 / 3 </Text>
-                </TouchableHighlight>
-              </View>
+        {/* put a number icon below the explained icon, such that the user gets attention on the specific icon */}
+        <IconMaterialCommunityIcons name='numeric-3-box' size={hp('7%')} color='#C92732' style={styles.tutorialMapArrowStyle}/>        
 
-              <View style={styles.stationRowContainer}>
-                <TouchableHighlight onPress={() => this.props.navigation.navigate('Tutorial5')} underlayColor="rgba(96,100,109, 1)" style={styles.unchosenButtonStyle}>
-                      <Text style={styles.anyButtonText}>Weiter </Text>
-                </TouchableHighlight>
-              </View>
-
-            </View>
-
-            <IconMaterialCommunityIcons name='numeric-3-box' size={hp('7%')} color='#C92732' style={styles.tutorialMapArrowStyle}/>
-            
-
-            </View>
-
-   
-        );
-      }          
+      </View>
+    );
+  }          
 }
