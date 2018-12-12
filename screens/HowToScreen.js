14ff4e374640d8e3fd6b4cc0b447768d001fbafe
kiserm <markus.kiser@hotmail.com>
2018-12-12 17:21:05 +0100

    proof of concept with audiofile is right and is working

--- screens/HowToScreen.js
+++ screens/HowToScreen.js
@@ -18,77 +18,6 @@ export default class HowToScreen extends React.Component {
     header: null, // do not show the navigation header, because the functions should not be available yet
   };
 
-  constructor(props) {
-    super(props);
-    // create a new sound object
-    this.howToTextSound = new Audio.Sound();
-    this.state = {
-      // if the flag is true, the play method should be invoked, else the replay method
-      audioIsPaused : false, 
-    }
-  };
-
-  /**
-   * IDEA:
-   * make that the sound is also playing on ios even if the phone is on silent mode
-   * this is done by setting the playsInSilentModeIOS to true, for more information look in the 
-   * definition of the modes. IMPORTANT: all modes have to be set, otherwise you get an error.
-  *//*
-  async prepareSound(){
-    // 
-    await Expo.Audio.setIsEnabledAsync(true);
-    await Expo.Audio.setAudioModeAsync({
-      playsInSilentModeIOS: true,
-      allowsRecordingIOS: false,
-      interruptionModeIOS: Expo.Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
-      shouldDuckAndroid: false,
-      interruptionModeAndroid: Expo.Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
-      playThroughEarpieceAndroid: false,
-    });
-  }
-
-  /**
-   * IDEA: 
-   * if the component will mount, load the audio file into the sound object
-   *//*
-  async componentWillMount(){
-    await this.howToTextSound.loadAsync(require('../assets/sounds/abc.mp3'));
-  };
-
-  /**
-   * IDEA:
-   * this method should continue to play the audio if the is paused flag is true, else
-   * it should restart playing the audio from the beginning
-   *//*
-  async playFunction(){ 
-    if(this.state.audioIsPaused){
-      this.prepareSound();
-      this.setState({audioIsPaused:false});
-      await this.howToTextSound.playAsync();
-    }   
-    else{
-      this.prepareSound();
-      await this.howToTextSound.replayAsync();
-    }
-  };
-
-  /**
-   * IDEA:
-   * this method pause the audio
-   *//*
-  async pauseFunction(){
-    await this.howToTextSound.pauseAsync();
-    this.setState({audioIsPaused:true});
-  };
-
-  /**
-   * IDEA: 
-   * this method stops the audio
-   *//*
-  async stopFunction(){
-    await this.howToTextSound.stopAsync();
-  };
-*/
   render() {
     // differ between iOS and Android since on Android the textAlign:justify is only available through the workaround over a webview
     if(Platform.OS === 'ios') {
@@ -205,7 +134,7 @@ export default class HowToScreen extends React.Component {
           </View>
           {/* show the navigation button to station 1 since the tutorialFlag is true*/}
           <View style={styles.howToNextScreenContainer}>
-            <TouchableHighlight onPress={() => {AudioFile.audioFunction('HowTo','stop');this.props.navigation.navigate('Station1')}} underlayColor="rgba(96,100,109, 1)" style={styles.howToButtonStyle}>
+            <TouchableHighlight onPress={() => {AudioFile.audioFunction('HowTo','pause');this.props.navigation.navigate('Station1')}} underlayColor="rgba(96,100,109, 1)" style={styles.howToButtonStyle}>
               <Text style={styles.howToButtonText} numberOfLines={1}>
                 zur Station 1 
               </Text>
@@ -231,7 +160,7 @@ export default class HowToScreen extends React.Component {
           </View>
           {/* show the navigation button to tutorial1 since the tutorialFlag is false */}
           <View style={styles.howToNextScreenContainer}>
-            <TouchableHighlight onPress={() => {this.stopFunction();this.props.navigation.navigate('Tutorial1')}} underlayColor="rgba(96,100,109, 1)" style={styles.howToButtonStyle}>
+            <TouchableHighlight onPress={() => {AudioFile.audioFunction('HowTo','pause');this.props.navigation.navigate('Tutorial1')}} underlayColor="rgba(96,100,109, 1)" style={styles.howToButtonStyle}>
               <Text style={styles.howToButtonText} numberOfLines={1}>
                 zum Tutorial 
               </Text>
