d7896dd4ea8d3d6b56e1831c28cdf120d83408eb
kiserm <markus.kiser@hotmail.com>
2018-12-18 17:13:51 +0100

    app.js done commenting

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
 import React from 'react';
+
+// import this handler to be able to turn off the android back button!
 import {BackHandler} from 'react-native';
+
 // import the self defined NavigationStack from the following document
 import NavigationStack from './navigation/StackNavigator';
 
 export default class App extends React.Component {
+    /**
+     * IDEA:
+     * with these three methods we can turn off the back button on android during the app, 
+     * such that after the user submitted his solutions, he can not go back anymore. this should be even impossible with the
+     * android default back button.
+     */
     componentDidMount() {
         BackHandler.addEventListener('hardwareBackPress', this.handleBackButton);
-      }
-      
+      }      
       componentWillUnmount() {
         BackHandler.removeEventListener('hardwareBackPress', this.handleBackButton);
       }      
       handleBackButton() {
         return true;
       }
-  render() {
-      return (
-          <NavigationStack />
-      );
+    
+    // launch the app by calling the StackNavigator with its initial screen
+    render() {
+        return (
+            <NavigationStack />
+        );
   }
 }
   
